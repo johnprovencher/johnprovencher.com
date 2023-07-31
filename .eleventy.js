434db9ec119058392ca6e74d85881534d1c01886
John Provencher <johnp5210@gmail.com>
2023-07-31 10:13:23 -0400

    x

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,20 @@
 const { DateTime } = require("luxon")
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('./src/style.css')
-    eleventyConfig.addPassthroughCopy('./src/script.js')
-    eleventyConfig.addPassthroughCopy('./src/assets')
-    eleventyConfig.addPassthroughCopy('./src/admin')
-    eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
-    eleventyConfig.addGlobalData('lore', '.src/data/lore.json');
+	eleventyConfig.addPassthroughCopy('./src/style.css')
+	eleventyConfig.addPassthroughCopy('./src/script.js')
+	eleventyConfig.addPassthroughCopy('./src/assets')
+	eleventyConfig.addPassthroughCopy('./src/admin')
+	eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
 
+	eleventyConfig.addFilter('postDate', (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+	})
 
-    eleventyConfig.addFilter('postDate', (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-    })
-
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    }
+	return {
+		dir: {
+			input: "src",
+			output: "public"
+		}
+	}
 }
