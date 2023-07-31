f67499f541deb61f254bad37fd1c5f4875b745b7
John Provencher <johnp5210@gmail.com>
2023-07-31 09:44:35 -0400

    x

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,30 @@
 const { DateTime } = require("luxon")
 
 module.exports = function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('./src/style.css')
-	eleventyConfig.addPassthroughCopy('./src/script.js')
-	eleventyConfig.addPassthroughCopy('./src/assets')
-	eleventyConfig.addPassthroughCopy('./src/admin')
-	eleventyConfig.addPassthroughCopy('./src/data/lore.json')
-	eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
+    eleventyConfig.addPassthroughCopy('./src/style.css')
+    eleventyConfig.addPassthroughCopy('./src/script.js')
+    eleventyConfig.addPassthroughCopy('./src/assets')
+    eleventyConfig.addPassthroughCopy('./src/admin')
+    eleventyConfig.addPassthroughCopy('./src/data/lore.json')
+    eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
 
-	eleventyConfig.addFilter('postDate', (dateObj) => {
-		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-	})
+    // Custom function to load data from 'data' folder
+    eleventyConfig.addDataExtension("json", (contents) => {
+        return JSON.parse(contents);
+    });
 
-	return {
-		dir: {
-			input: "src",
-			output: "public"
-		}
-	}
+    // Set the data directory to 'data'
+    eleventyConfig.setDataDeepMerge(true);
+    eleventyConfig.setDataDir("data");
+    
+    eleventyConfig.addFilter('postDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    })
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
 }
